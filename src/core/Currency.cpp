74f488cd97d4bb933841ddcdf8d286742cf4b878
Shiro <otakugroup2015@gmail.com>
2018-05-24 18:23:02 +0900

    back to genesis 1
    to rollback!

--- src/core/Currency.cpp
+++ src/core/Currency.cpp
@@ -113,7 +113,7 @@ bool Currency::generateGenesisBlock() {
     return false;
   }
 
-  m_genesisBlock.majorVersion = BLOCK_MAJOR_VERSION_4;
+  m_genesisBlock.majorVersion = BLOCK_MAJOR_VERSION_1;
   m_genesisBlock.minorVersion = BLOCK_MINOR_VERSION_0;
   m_genesisBlock.timestamp = GENESIS_TIMESTAMP;
   m_genesisBlock.nonce = GENESIS_NONCE;
